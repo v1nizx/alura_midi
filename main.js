e4b18c07dd95b1871c99403e21662ad2aae51bd9
v1nizx <144844828+v1nizx@users.noreply.github.com>
2024-04-29 08:51:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio){
+    
+    const elemento = document.querySelector(seletorAudio)
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else{
+        console.log('Elemento nao encontrado');
+    }
+
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
     
-    contador = contador + 1;
-   
-    console.log(contador);
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function (evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+ 
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
 
+}
